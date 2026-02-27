8c7f9b9a0b3d55a1150a5f1f4686a551144dff7c
CavarnaliA <nastea.cavarnali@gmail.com>
2026-02-27 10:25:32 +0200

    Add files via upload

---
+++ lab2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Привет, мир!";
+
+
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "Конкатенация: " . $days . " — " . $message . "<br />";
+echo "Двойные кавычки: $days — $message<br />";
